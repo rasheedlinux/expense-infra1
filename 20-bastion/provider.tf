9a785dbbb02567aafe4baa5a44ab69a5bf52789c
rasheedlinux <rasheed2737@gmail.com>
2026-03-31 23:57:48 +0530

    Upgrade AWS provider version in provider.tf
    Updated AWS provider version to 6.37.0 or higher.

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.66.0"
-    }
+  aws = {
+    source  = "hashicorp/aws"
+    version = ">= 6.37.0"
   }
+}
 
   backend "s3" {
     bucket = "expense-dev-396608782364-us-east-1-an"
